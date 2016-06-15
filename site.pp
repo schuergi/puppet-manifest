5c17803264fa21bc3541245fa76e47250b67de73
schuergi <schuergf@student.hu-berlin.de>
2016-06-15 16:34:23 +0200

    added new node, little experimenting

--- site.pp
+++ site.pp
@@ -2,24 +2,52 @@
 #node fai-server {
 #}
 
+node fai-client {
+
+package { 'pairs':
+	ensure => installed,
+}
+
+}
+
 node fai-server {
 	# Configure puppetdb and its underlying database
-	class { 'puppetdb': }
+#	class { 'puppetdb': 
+#		database => 'embedded',
+#		database_embedded_path => '/var/lib/puppetdb/',
+#	#	disable_ssl => 'true',
+#		ssl_listen_address => '0.0.0.0',
+#		}
 	
 	# Configure the Puppet master to use puppetdb
-	class { 'puppetdb::master::config': }
+	class { 'puppetdb::master::config': 
+		puppetdb_server => 'fai-server.lan',
+  		puppetdb_port   => 8081,
+		}
 
 	class { 'puppetboard':
 	  manage_git        => true,
 	  manage_virtualenv => true,
 		}
 
-	class { 'apache': }
+	class { 'apache': 
+		mpm_module => 'prefork',
+		purge_configs => 'false',
+		}
+
 	class { 'apache::mod::wsgi': }
+	class { 'apache::mod::headers': }
+	class { 'apache::mod::rewrite': }
+	class { 'apache::mod::php': }
+	class { 'apache::mod::proxy': 
+		allow_from => '0.0.0.0',
+		proxy_requests => 'On'
+		}
 	# Configure Puppetboard
 	# Access Puppetboard through pboard.example.com
 	class { 'puppetboard::apache::vhost':
 		 vhost_name => 'puppetboard.fai-server.lan',
 		 port       => 80,
 		}
-	}
+
+}
