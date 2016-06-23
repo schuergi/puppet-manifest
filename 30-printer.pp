7a1fb9583206910900cca98cf8e975d7f57a465a
Felix Schuergut <schuergf@student.hu-berlin.de>
2016-06-23 13:37:12 +0200

    added ppd for printers, set up raw queue

--- 30-printer.pp
+++ 30-printer.pp
@@ -1,7 +1,6 @@
 class printer::judith {
 
 
-# include 'cups::'
 	class { '::cups':
 	  default_queue => 'KONICA_MINOLTA_362_282_222',
 		}
@@ -20,29 +19,35 @@ class printer::judith {
 
 	cups_queue { 'Kyocera-Kyocera-FS-4200DN':
 		ensure 	=> 'printer',
-		uri 	=> 'socket://192.168.1.190:9100',
+		uri 	=> 'ipp://192.168.1.199/ipp',
 		options => {
 			'accepting'	=> 'true',
 			'enabled'	=> 'true',
-			'PageSize'	=> 'A4',
-			'Duplex'	=> 'true',
-			'Resolution'	=> '600dpi',
+			'make_and_model' => 'raw',
 			}
 		
 		}
 		
+	file { '/usr/share/ppd/custom/KO362U.ppd':
+	        source => 'puppet:///files/KO362U.ppd',
+	        owner => 'root',
+	        group => 'root',
+	        mode => '644',
+	        ensure => 'file',
+		}
 	
 }
 
 class printer::kyocera {
-# include 'cups::'
+
 	class { '::cups':
-	  default_queue => 'Kyocera-Kyocera-FS-4200DN',
+		default_queue => 'Kyocera-Kyocera-FS-4200DN',
 		}
 	
 	cups_queue { 'KONICA_MINOLTA_362_282_222':
 		ensure 	=> 'printer',
 		uri 	=> 'socket://192.168.1.130',
+		ppd	=> '/usr/share/ppd/custom/KO362U.ppd',
 		options => {
 			'accepting'	=> 'true',
 			'enabled'	=> 'true',
@@ -53,18 +58,25 @@ class printer::kyocera {
 		}
 
 	cups_queue { 'Kyocera-Kyocera-FS-4200DN':
-		ensure 	=> 'printer',
-		uri 	=> 'socket://192.168.1.190:9100',
-		options => {
-			'accepting'	=> 'true',
-			'enabled'	=> 'true',
-			'PageSize'	=> 'A4',
-			'Duplex'	=> 'true',
-			'Resolution'	=> '600dpi',
-			}
-		
-		}
-		
+                ensure  => 'printer',
+                uri     => 'ipp://192.168.1.199/ipp',
+                make_and_model => 'raw',
+                options => {
+                        'accepting'     => 'true',
+                        'enabled'       => 'true',
+                        }
+
+                }
+
+
+	file { '/usr/share/ppd/custom/KO362U.ppd':
+	        source => 'puppet:///files/KO362U.ppd',
+	        owner => 'root',
+	        group => 'root',
+	        mode => '644',
+	        ensure => 'file',
 	
+		}
 
 }
+
